cc9c23554dbad8cbe0792def748a18557421b5e6
Ivan Andriukha <64382696+JackGhoster@users.noreply.github.com>
2023-11-01 23:55:44 +0400

    Intro: Add text to slides

--- Assets/GlnogStuff/_Scripts/Intro/IntroConfig.cs
+++ Assets/GlnogStuff/_Scripts/Intro/IntroConfig.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "IntroConfig", menuName = "Configs/Intro/IntroConfig")]
 public class IntroConfig : ScriptableObject {
-    [field: SerializeField] public Sprite[] ComicSprites { get; private set; }   
+    [field: SerializeField] public Sprite[] ComicSprites { get; private set; }
+    [field: SerializeField] public string[] ComicStrings { get; private set; }
 }
